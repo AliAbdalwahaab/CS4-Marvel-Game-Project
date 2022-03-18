2cc698eae07274fb872f874be844a25b5ad9a78f
Omar-Nour <omar.syd.n@gmail.com>
2022-03-18 22:28:32 +0200

    Fix Random Calculation

--- src/model/world/Cover.java
+++ src/model/world/Cover.java
@@ -8,7 +8,7 @@ public class Cover {
     private Point location;
 
     public Cover(int x, int y) {
-        currentHP = (int) Math.random()*1000+1;
+        currentHP = (int) (Math.random()*1000+1);
         location = new Point(x,y);
     }
 
