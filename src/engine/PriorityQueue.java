a9b6e858c3d4545b1987f90af1791fcccca9c19e
Omar-Nour <52005044+Omar-Nour@users.noreply.github.com>
2022-03-18 15:27:15 +0000

    Update PriorityQueue.java

--- src/engine/PriorityQueue.java
+++ src/engine/PriorityQueue.java
@@ -13,7 +13,7 @@ class PriorityQueue {
    public void insert(Comparable item) {
       
       int i;
-      for (i = nItems - 1;i >= 0 && item.compareTo(elements[i]) > 0;i--)
+      for (i = nItems - 1; i >= 0 && item.compareTo(elements[i]) > 0; i--)
          elements[i + 1] = elements[i];
 
       elements[i + 1] = item;
