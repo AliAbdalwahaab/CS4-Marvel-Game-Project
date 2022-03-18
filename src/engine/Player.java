bd19afc038a30af40c12c26b3346616c60b9cdb7
Mohamed Shamekh <shamekhjr@gmail.com>
2022-03-18 22:48:47 +0200

    Built the Player class in engine package

---
+++ src/engine/Player.java
@@ -0,0 +1,36 @@
+package engine;
+
+import model.world.Champion;
+
+import java.util.ArrayList;
+
+public class Player {
+    //Attributes
+    private String name; //READ ONLY
+    private Champion leader; //READ and WRITE
+    private ArrayList<Champion> team; //READ ONLY
+
+    //Constructor
+    public Player(String name) {
+        this.name = name;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public Champion getLeader() {
+        return leader;
+    }
+
+    public ArrayList<Champion> getTeam() {
+        return team;
+    }
+
+
+    //Setters
+    public void setLeader(Champion leader) {
+        this.leader = leader;
+    }
+}
