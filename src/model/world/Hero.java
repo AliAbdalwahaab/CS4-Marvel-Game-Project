a661878d47c3e72bac730cdc589989ce94a49366
Mohamed Shamekh <shamekhjr@gmail.com>
2022-03-20 16:57:14 +0200

    Added a getter for heroClass in Hero.java

--- src/model/world/Hero.java
+++ src/model/world/Hero.java
@@ -11,4 +11,7 @@ public class Hero extends Champion {
     public Hero(String name, int maxHP, int mana, int maxActions, int speed, int attackRange, int attackDamage) {
         super(name, maxHP, mana, maxActions, speed, attackRange, attackDamage);
     }
-}
+    
+    public HeroClass getHeroClass() {
+        return HEROCLASS;
+    }
