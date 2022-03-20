dda7e82d34dfab14a213c2855f67452d86e4112b
Mohamed Shamekh <shamekhjr@gmail.com>
2022-03-20 16:57:48 +0200

    Added getter for heroClass in Anti-Hero.java

--- src/model/world/AntiHero.java
+++ src/model/world/AntiHero.java
@@ -11,4 +11,8 @@ public class AntiHero extends Champion {
     public AntiHero(String name, int maxHP, int mana, int maxActions, int speed, int attackRange, int attackDamage) {
         super(name, maxHP, mana, maxActions, speed, attackRange, attackDamage);
     }
+    
+    public HeroClass getHeroClass() {
+        return HEROCLASS;
+    }
 }
