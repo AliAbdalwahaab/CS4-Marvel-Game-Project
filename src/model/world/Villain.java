0c9b46269a45eaf9148a2e78a95ff9e19da84674
Mohamed Shamekh <shamekhjr@gmail.com>
2022-03-20 16:58:23 +0200

    Added getter method for heroClass in Villain.java

--- src/model/world/Villain.java
+++ src/model/world/Villain.java
@@ -11,5 +11,9 @@ public class Villain extends Champion {
     public Villain(String name, int maxHP, int mana, int maxActions, int speed, int attackRange, int attackDamage) {
         super(name, maxHP, mana, maxActions, speed, attackRange, attackDamage);
     }
+    
+    public HeroClass getHeroClass() {
+        return HEROCLASS;
+    }
 
 }
