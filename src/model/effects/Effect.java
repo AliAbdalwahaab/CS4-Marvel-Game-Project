2d4994add12425435df1a883c43705fe2304ed48
Omar-Nour <omar.syd.n@gmail.com>
2022-03-21 00:53:31 +0200

    Convert to abstract and add setters and getters

--- src/model/effects/Effect.java
+++ src/model/effects/Effect.java
@@ -1,16 +1,33 @@
 package model.effects;
 
-public class Effect {
+abstract public class Effect {
 
-    private String name; // READ ONLY
-    private int duration; // READ AND WRITE
-    private EffectType type; // READ ONLY
+    private String name; // R
+    private int duration; // RW
+    private EffectType type; // R
 
-    public Effect(String name, int duration, EffectType type){
+    public Effect(String name, int duration, EffectType type) {
       this.name = name;
       this.duration = duration;
       this.type = type;
-
-
     }
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public EffectType getType() {
+        return this.type;
+    }
+
+    // setters
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
 }
