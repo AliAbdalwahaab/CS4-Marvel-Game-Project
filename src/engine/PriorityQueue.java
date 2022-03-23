3bd2da33badcb225a288d57d69ac20dc872a5196
Omar-Nour <omar.syd.n@gmail.com>
2022-03-23 23:44:52 +0200

    Modify PriorityQueue to be a part of the engine package

--- src/engine/PriorityQueue.java
+++ src/engine/PriorityQueue.java
@@ -1,5 +1,7 @@
+package engine;
+
 class PriorityQueue {
- 
+
    private Comparable[] elements;
    private int nItems;
    private int maxSize;
@@ -9,9 +11,9 @@ class PriorityQueue {
       elements = new Comparable[maxSize];
       nItems=0;
    }
-    
+
    public void insert(Comparable item) {
-      
+
       int i;
       for (i = nItems - 1; i >= 0 && item.compareTo(elements[i]) > 0; i--)
          elements[i + 1] = elements[i];
@@ -19,24 +21,24 @@ class PriorityQueue {
       elements[i + 1] = item;
       nItems++;
    }
-    
+
    public Comparable remove() {
       nItems--;
       return elements[nItems];
    }
-    
+
    public boolean isEmpty() {
       return (nItems == 0);
    }
-    
+
    public boolean isFull() {
       return (nItems == maxSize);
    }
-  
+
    public Comparable peekMin() {
       return elements[nItems-1];
    }
-     
+
    public int size() {
       return nItems;
    }
