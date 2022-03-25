ef0a85889d0c9b77c8c52cb464852886460c53ff
Mohamed Shamekh <shamekhjr@gmail.com>
2022-03-25 19:11:53 +0200

    Modified speedup class

--- src/model/effects/SpeedUp.java
+++ src/model/effects/SpeedUp.java
@@ -2,9 +2,7 @@ package model.effects;
 
 public class SpeedUp extends Effect {
 
-    public SpeedUp(String name, int duration, EffectType type){
-        super(name, duration, type);
-    }
+
 
     public SpeedUp(String name, int duration) {
 
