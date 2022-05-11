d3707e2f407b5d8636adf1bbbe720e6c4b845716
Mohamed Shamekh <shamekhjr@gmail.com>
2022-05-11 16:41:16 +0200

    Not sure if I have to apply Embrace effect on leader

--- src/model/world/Hero.java
+++ src/model/world/Hero.java
@@ -39,6 +39,8 @@ public class Hero extends Champion {
                 Embrace e = new Embrace(2);
                 e.apply(targets.get(i));
             }
+            //Embrace e = new Embrace(2);
+            //e.apply(this);
         }
     }
 }
