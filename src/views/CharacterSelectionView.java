cb0ed0a5b516a1cd6d89a93409a92d4819f88e82
Mohamed Shamekh <shamekhjr@gmail.com>
2022-05-30 00:09:01 +0200

    updated character selection class, much better now

--- src/views/CharacterSelectionView.java
+++ src/views/CharacterSelectionView.java
@@ -86,94 +86,141 @@ public class CharacterSelectionView extends JFrame implements ActionListener {
 
 			this.getContentPane().removeAll();
 			this.repaint();
-			this.setLayout(new BorderLayout());
-			this.setVisible(true);
 
-			centerPanel = new JPanel();
-			centerPanel.setLayout(new GridLayout(0,5));
+			updateTeams();
 
-			btns = new ArrayList<JButton>();
-
-			try {
-				currentGame.loadChampions("Champions.csv");
-				currentGame.loadAbilities("Abilities.csv");
-			} catch (Exception exception) {
-				System.out.println("Error");
-			}
-
-			for (Champion c: currentGame.getAvailableChampions()) {
-				JButton b = new JButton();
-				b.setText(c.getName());
-				b.addActionListener(this);
-				centerPanel.add(b);
-				btns.add(b);
-			}
-
-			this.add(centerPanel,BorderLayout.CENTER);
-			centerPanel.setVisible(true);
-			this.revalidate();
-			this.repaint();
-
-
-			topPanel = new JPanel();
-			topPanel.setLayout(new GridLayout(0,2));
-			topPanel.setVisible(true);
-
-			JTextArea player1Name = new JTextArea();
-			player1Name.setText(currentGame.getFirstPlayer().getName());
-			player1Name.setVisible(true);
-			player1Name.setEditable(false);
-
-			JTextArea player1Leader = new JTextArea();
-			player1Leader.setText("lol");
-			player1Leader.setVisible(true);
-			player1Leader.setEditable(false);
-
-			JTextArea player1Champ1 = new JTextArea();
-			player1Champ1.setText("currentGame.getFirstPlayer().getTeam().get(1).getName()");
-			player1Champ1.setVisible(true);
-			player1Champ1.setEditable(false);
-
-			JTextArea player1Champ2 = new JTextArea();
-			player1Champ2.setText("currentGame.getFirstPlayer().getTeam().get(2).getName()");
-			player1Champ2.setVisible(true);
-			player1Champ2.setEditable(false);
-
-			JPanel player1Panel = new JPanel();
-			player1Panel.setLayout(new GridLayout(4,0));
-
-			player1Panel.add(player1Name);
-			player1Panel.add(player1Leader);
-			player1Panel.add(player1Champ1);
-			player1Panel.add(player1Champ2);
-			this.revalidate();
-			this.repaint();
-
-			player1Panel.setVisible(true);
-
-
-
-
-
-
-			JTextArea player2Name = new JTextArea();
-			player1Name.setText("currentGame.getSecondPlayer().getName()");
-			JTextArea player2Leader = new JTextArea();
-			player1Name.setText("currentGame.getSecondPlayer().getLeader().getName()");
-			JTextArea player12hamp1 = new JTextArea();
-			player1Name.setText("currentGame.getSecondPlayer().getTeam().get(1).getName()");
-			JTextArea player2Champ2 = new JTextArea();
-			player1Name.setText("currentGame.getSecondPlayer().getTeam().get(2).getName()");
-
-
-			this.add(topPanel,BorderLayout.NORTH);
-			this.revalidate();
-			this.repaint();
-
-			
 		}
 
+
+
 		
+	}
+
+	public void updateTeams() {
+
+		this.setLayout(new BorderLayout());
+		this.setVisible(true);
+
+		centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(0,5));
+
+		btns = new ArrayList<JButton>();
+
+		try {
+			currentGame.loadChampions("Champions.csv");
+			currentGame.loadAbilities("Abilities.csv");
+		} catch (Exception exception) {
+			System.out.println("Error");
+		}
+
+		for (Champion c: currentGame.getAvailableChampions()) {
+			JButton b = new JButton();
+			b.setText(c.getName());
+			b.addActionListener(this);
+			centerPanel.add(b);
+			btns.add(b);
+		}
+
+		this.add(centerPanel,BorderLayout.CENTER);
+		centerPanel.setVisible(true);
+		this.revalidate();
+		this.repaint();
+
+
+		topPanel = new JPanel();
+		topPanel.setLayout(new GridLayout(0,2));
+		topPanel.setPreferredSize(new Dimension(200,200));
+		topPanel.setVisible(true);
+
+		JTextArea player1Name = new JTextArea();
+		player1Name.setText("  "+currentGame.getFirstPlayer().getName());
+		player1Name.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+
+		player1Name.setVisible(true);
+		player1Name.setEditable(false);
+
+		JTextArea player1Leader = new JTextArea();
+		player1Leader.setText("   lol");
+		player1Leader.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+		player1Leader.setVisible(true);
+		player1Leader.setEditable(false);
+
+		JTextArea player1Champ1 = new JTextArea();
+		player1Champ1.setText("    currentGame.getFirstPlayer().getTeam().get(1).getName()");
+		player1Champ1.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+		player1Champ1.setVisible(true);
+		player1Champ1.setEditable(false);
+
+		JTextArea player1Champ2 = new JTextArea();
+		player1Champ2.setText("    currentGame.getFirstPlayer().getTeam().get(2).getName()");
+		player1Champ2.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+		player1Champ2.setVisible(true);
+		player1Champ2.setEditable(false);
+
+		JPanel player1Panel = new JPanel();
+		player1Panel.setLayout(new GridLayout(4,0));
+		player1Panel.setVisible(true);
+		player1Panel.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),topPanel.getHeight()));
+
+		player1Panel.add(player1Name);
+		player1Panel.add(player1Leader);
+		player1Panel.add(player1Champ1);
+		player1Panel.add(player1Champ2);
+
+		topPanel.add(player1Panel);
+
+		this.revalidate();
+		this.repaint();
+
+
+
+
+		JPanel player2Panel = new JPanel();
+		player2Panel.setLayout(new GridLayout(4,0));
+		player2Panel.setVisible(true);
+
+		player2Panel.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),topPanel.getHeight()));
+
+		JTextArea player2Name = new JTextArea();
+		player2Name.setText(currentGame.getSecondPlayer().getName());
+		player2Name.setEditable(false);
+		player2Name.setVisible(true);
+		player2Name.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+
+
+		JTextArea player2Leader = new JTextArea();
+		player2Leader.setText("currentGame.getSecondPlayer().getLeader().getName()");
+		player2Leader.setEditable(false);
+		player2Leader.setVisible(true);
+		player2Leader.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+
+
+		JTextArea player2Champ1 = new JTextArea();
+		player2Champ1.setText("currentGame.getSecondPlayer().getTeam().get(1).getName()");
+		player2Champ1.setEditable(false);
+		player2Champ1.setVisible(true);
+		player2Champ1.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+
+		JTextArea player2Champ2 = new JTextArea();
+		player2Champ2.setText("currentGame.getSecondPlayer().getTeam().get(2).getName()");
+		player2Champ2.setEditable(false);
+		player2Champ2.setVisible(true);
+		player2Champ2.setPreferredSize(new Dimension((int) 0.5*topPanel.getWidth(),20));
+
+		player2Panel.add(player2Name);
+		player2Panel.add(player2Leader);
+		player2Panel.add(player2Champ1);
+		player2Panel.add(player2Champ2);
+
+		topPanel.add(player2Panel);
+
+		this.revalidate();
+		this.repaint();
+
+
+		this.add(topPanel,BorderLayout.NORTH);
+		this.revalidate();
+		this.repaint();
 	}
 
 }
