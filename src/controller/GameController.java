6f0bf73707be3eef997269d7c94530db42edc327
Omar-Nour <omar.syd.n@gmail.com>
2022-05-30 23:29:16 +0200

    South Panel in Game View

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -2,10 +2,77 @@ package controller;
 
 import engine.Game;
 
-public class GameController {
+import java.awt.event.*;
+
+public class GameController implements ActionListener, MouseListener, WindowListener {
     private Game game;
 
     public GameController(Game g) {
         //TODO
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //USELESS
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
     }
 }
--- src/views/GameVIEW.java
+++ src/views/GameVIEW.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 import engine.Player;
 
-public class GameVIEW extends JFrame {
+public class GameVIEW extends JFrame{
     private JPanel GameBoard;
     private JPanel Abilities;
     private JButton CCAbility;
@@ -19,11 +19,43 @@ public class GameVIEW extends JFrame {
 
     public GameVIEW(GameController controller){
         setTitle("GAME STARTED!");
+        addMouseListener(controller);
         setVisible(true);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setBounds(0, 0, 1920, 1080);
+
+
+        //Board (CENTER)
+
+        // Abilities left (2 grids)
+
+        // TurnOrder + Movement &  Attack right (2 grids)
+
+        // Hover Info (Page Start)
+
+        // Current Champ info (Page End)
+        JPanel CurrentChampInfo =  new JPanel(new GridLayout(2, 4));
+        JLabel ChampName = new JLabel("Karinge");
+        JLabel ChampType = new JLabel("Karinge");
+        JLabel ChampHP = new JLabel("Karinge");
+        JLabel ChampMana = new JLabel("Karinge");
+        JLabel ChampActionPoints = new JLabel("Karinge");
+        JLabel ChampAttackDmg = new JLabel("Karinge");
+        JComboBox ChampAbilities = new JComboBox();
+        JComboBox ChampAppliedEffects = new JComboBox();
+        CurrentChampInfo.add(ChampName);
+        CurrentChampInfo.add(ChampType);
+        CurrentChampInfo.add(ChampHP);
+        CurrentChampInfo.add(ChampMana);
+        CurrentChampInfo.add(ChampActionPoints);
+        CurrentChampInfo.add(ChampAttackDmg);
+        CurrentChampInfo.add(ChampAbilities);
+        CurrentChampInfo.add(ChampAppliedEffects);
+        CurrentChampInfo.setVisible(true);
+        this.add(CurrentChampInfo, BorderLayout.SOUTH);
+
+
         GameBoard = new JPanel();
         GameBoard.setLayout(new GridLayout(5, 5));
         add(GameBoard, BorderLayout.CENTER);
@@ -38,6 +70,9 @@ public class GameVIEW extends JFrame {
         Abilities.add(HAbility);
         Abilities.add(LAbility);
         add(Abilities,BorderLayout.EAST);
+
+        this.revalidate();
+        this.repaint();
     }
 
 }
