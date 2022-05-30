9b7d42d6a0c3e4e584d3e86cf0814294131c5761
Omar-Nour <omar.syd.n@gmail.com>
2022-05-30 19:48:07 +0200

    1920 GIF

--- src/views/StartScreenGUI.java
+++ src/views/StartScreenGUI.java
@@ -1,6 +1,8 @@
 package views;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -9,18 +11,23 @@ import javax.swing.*;
 
 import views.CharacterSelectionView.*;
 
-public class StartScreenGUI extends JComponent {
+public class StartScreenGUI extends JComponent implements ActionListener {
     JPanel contentPane;
     JLabel imageLabel = new JLabel();
     JLabel headerLabel = new JLabel();
+    JFrame playPhase;
+    JButton play;
 
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        new StartScreenGUI();
+    }
+    public StartScreenGUI() throws MalformedURLException, InterruptedException {
         JFrame f = new JFrame();
 
         f.setLayout(new BorderLayout());
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        Icon icon = new ImageIcon("THH-LOGO_F2.gif");
+        Icon icon = new ImageIcon("THH-LOGO.gif");
         JLabel label = new JLabel();
         label.setIcon(icon);
         f.setContentPane(label);
@@ -29,10 +36,25 @@ public class StartScreenGUI extends JComponent {
         f.setVisible(true);
         TimeUnit.SECONDS.sleep(22);
 
-        new CharacterSelectionView();
+        f.setVisible(false);
+        JFrame playPhase = new JFrame();
+        playPhase.setLayout(new BorderLayout());
+        playPhase.setVisible(true);
+        play = new JButton("Play");
+        play.addActionListener(this);
+
+        playPhase.add(play, BorderLayout.PAGE_END);
+        playPhase.setExtendedState(JFrame.MAXIMIZED_BOTH);
         f.dispose();
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == play) {
+            playPhase.dispose();
+            new CharacterSelectionView();
+        }
+    }
 }
 
